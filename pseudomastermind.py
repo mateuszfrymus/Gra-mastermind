b1c7818876ebbcbf17093dfd407447222809dfef
mateusz462 <mateusz462@gmail.com>
2020-02-02 15:50:50 +0100

    add new line "Witaj w grze mastermind"

--- pseudomastermind.py
+++ pseudomastermind.py
@@ -15,8 +15,9 @@ list.remove(c)
 d = random.choice(list)
 randList.append(d)
 list.remove(d)
-#print(list)
-#print(randList)
+# print(list)
+# print(randList)
+print("Witaj w grze mastermind!")
 
 while True:
     print(""" 1. Losuj
@@ -24,45 +25,44 @@ while True:
     wybor = int(input())
     if wybor == 1:
         while True:
-        
-                while True:
-                    print("Wprowadź 1. liczbę:  ")
-                    liczba1 = int(input())
-                    if liczba1 == randList[0]:
-                        print("trafiles")
-                        print("Pierwsza liczba to:", liczba1)
-                        break
-                    else:
-                        print("Nie trafiłeś. Spróbuj ponownie")
-                while True:
-                    print("Wprowadź 2. liczbę:  ")
-                    liczba2 = int(input())
-                    if liczba2 == randList[1]:
-                        print("trafiles")
-                        print("Druga liczba to:", liczba2)
-                        break
-                    else:
-                        print("Nie trafiłeś. Spróbuj ponownie")
-                while True:
-                    print("Wprowadź 3. liczbę:  ")
-                    liczba3 = int(input())
-                    if liczba3 == randList[2]:
-                        print("trafiles")
-                        print("Trzecia liczba to:", liczba3)
-                        break
-                    else:
-                        print("Nie trafiłeś. Spróbuj ponownie")
-                while True:
-                    print("Wprowadź 4. liczbę:  ")
-                    liczba4 = int(input())
-                    if liczba4 == randList[3]:
-                        print("trafiles")
-                        print("Czwarta liczba to:", liczba4)
-                        print("Wygrałeś! Twoja sekwencja liczb to:", randList)
-                        break
-                    else:
-                        print("Nie trafiłeś. Spróbuj ponownie")
-          
-                        
+
+            while True:
+                print("Wprowadź 1. liczbę:  ")
+                liczba1 = int(input())
+                if liczba1 == randList[0]:
+                    print("trafiles")
+                    print("Pierwsza liczba to:", liczba1)
+                    break
+                else:
+                    print("Nie trafiłeś. Spróbuj ponownie")
+            while True:
+                print("Wprowadź 2. liczbę:  ")
+                liczba2 = int(input())
+                if liczba2 == randList[1]:
+                    print("trafiles")
+                    print("Druga liczba to:", liczba2)
+                    break
+                else:
+                    print("Nie trafiłeś. Spróbuj ponownie")
+            while True:
+                print("Wprowadź 3. liczbę:  ")
+                liczba3 = int(input())
+                if liczba3 == randList[2]:
+                    print("trafiles")
+                    print("Trzecia liczba to:", liczba3)
+                    break
+                else:
+                    print("Nie trafiłeś. Spróbuj ponownie")
+            while True:
+                print("Wprowadź 4. liczbę:  ")
+                liczba4 = int(input())
+                if liczba4 == randList[3]:
+                    print("trafiles")
+                    print("Czwarta liczba to:", liczba4)
+                    print("Wygrałeś! Twoja sekwencja liczb to:", randList)
+                    break
+                else:
+                    print("Nie trafiłeś. Spróbuj ponownie")
+
     elif wybor == 2:
         break
